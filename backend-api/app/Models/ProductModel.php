800461e7863960f1aeb590dc79519b81e15f83f1
dzaki111 <dzakiarifrahman636@gmail.com>
2026-06-14 22:11:18 +0700

    Delete backend-api directory

--- backend-api/app/Models/ProductModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-use CodeIgniter\Model;
-
-class ProductModel extends Model {
-    protected $table = 'products';
-    protected $primaryKey = 'id';
-    protected $allowedFields = ['category_id', 'product_name', 'sku', 'stock', 'price', 'supplier_name'];
-}
