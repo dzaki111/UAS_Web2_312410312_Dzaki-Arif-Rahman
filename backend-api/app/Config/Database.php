f080861cebf6b8d5d641edc0d29a2403d7b47d7a
dzaki111 <dzakiarifrahman636@gmail.com>
2026-06-14 23:50:49 +0700

    Update Database.php

--- backend-api/app/Config/Database.php
+++ backend-api/app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => 'sql3.freesqldatabase.com',
+        'username'     => 'sql3830465',
+        'password'     => 'QrBB8JFLnU',
+        'database'     => 'sql3830465',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
